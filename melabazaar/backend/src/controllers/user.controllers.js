7f2ca9c431080d54f63f067bca209175b8996cb2
Arnab Santra <arnabsantra365@gmail.com>
2024-06-03 16:57:53 +0530

    login and token method written

--- melabazaar/backend/src/controllers/user.controllers.js
+++ melabazaar/backend/src/controllers/user.controllers.js
@@ -2,6 +2,21 @@ import {asynchandler} from "../utils/asynchandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import { user } from "../models/users.models.js";
+
+const generateAccandRfr = async(userId)=>{
+    try {
+        const User=await user.findById(userId)
+        const accessToken = User.generateAccessToken();
+        const refreshToken = User.generateRefreshToken();
+        User.refreshToken = refreshToken
+        User.save({validateBeforeSave:false})
+
+        return {accessToken,refreshToken};
+    } catch (error) {
+        throw new ApiError(500,'Something went wrong');
+    }
+}
+
 const regUser = asynchandler(async (req,res)=>{
     // res.status(200).json({ //to register the user
     //     message:"ok",
@@ -46,4 +61,43 @@ return res.status(201).json(
 
 })
 
-export{regUser};
+const loginUser =asynchandler(async(req,res)=>{
+const {email,username,password} =req.body
+if(!username || !email){
+    throw new ApiError(400,"username and email required");
+}
+const User = await user.findOne({ //fin user by email or username
+    $or: [{username},{email}]
+})
+
+if(!User){ //checking if user exists or not
+    throw new ApiError(404,"user doesn't exist")
+}
+
+const isPassValid=await User.isPasswordCorrect (password);
+
+if(!isPassValid){
+    throw new ApiError(401,'Invalid password')
+}
+
+const {accessToken,refreshToken} = await generateAccandRfr(User._id);//get access and refresh token from the method called above
+
+const logUser = await user.findById(User._id).select(" -password -refreshToken");
+
+const options={
+    httpOnly : true,
+    secure :true
+}
+
+return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",refreshToken,options)
+.json(
+    new ApiResponse(
+        200,
+        {
+             User: logUser, accessToken, refreshToken
+        },
+        "User logged in successfully"
+    )
+)
+})
+export{regUser,loginUser};
--- melabazaar/backend/src/routes/user.routes.js
+++ melabazaar/backend/src/routes/user.routes.js
@@ -1,8 +1,8 @@
 import {Router} from "express";
-import {regUser} from "../controllers/user.controllers.js";
+import {loginUser, regUser} from "../controllers/user.controllers.js";
 const router= Router()
 
 router.route("/register").post(regUser) // this call goes to regUser of user controllerl
 // router.route("/login").post(registerUser)
-
+router.route("/login").post(loginUser);
 export default router;
